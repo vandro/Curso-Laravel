3b6061a6da660780e5684c6aa0a91fe6c132df4e
Vandro Laravel <vandro.nds@gmail.com>
2019-09-16 16:46:54 -0400

    Parte 8

---
+++ database/factories/PermisoFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Permiso;
+use Faker\Generator as Faker;
+
+$factory->define(Permiso::class, function (Faker $faker) {
+    return [
+        'nombre'=>$faker->name,
+        'slug'=>$faker->unique()->safeEmail,
+    ];
+});
