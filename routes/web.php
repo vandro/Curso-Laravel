0b8f03a94e2139f01433f54271cc876c7fc69ffb
Vandro Laravel <vandro.nds@gmail.com>
2019-09-19 17:26:42 -0400

    parte 11

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'InicioController@index');
 
-Route::get('permiso', 'PermisoController@index');
-
+//Route::get('permiso', 'PermisoController@index');
